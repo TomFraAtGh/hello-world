aa58dcd73699f6a41adc79d74db98950afa9707f
Tomas Frankkila <tomas_frankkila@yahoo.se>
2016-05-27 15:06:21 +0200

    Updated on Mac

--- hello-world/main.cpp
+++ hello-world/main.cpp
@@ -11,6 +11,7 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
-    std::cout << "Hi, this was added by editing the file on github.com\n"
+    std::cout << "Hi, this was added by editing the file on github.com\n";
+    std::cout << "Hi again, this was later added on the Mac\n";
     return 0;
 }
